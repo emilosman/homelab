cce592d07156fb9de5722a1f6671d2f03833d5bb
Emil Osmanbegović <hello@emilosman.com>
2024-07-17 11:32:20 +0200

    update hosts file

--- ad-hoc.sh
+++ ad-hoc.sh
@@ -1,3 +1,3 @@
-ansible -i hosts.ini kicomp-xyz -m ping -u emil
+ansible -i hosts kicomp-xyz -m ping -u emil
 
-ansible -i hosts.ini kicomp-xyz -a "free -h" -u emil
+ansible -i hosts kicomp-xyz -a "free -h" -u emil
