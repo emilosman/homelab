71bc40ed28da648074e135a3a24263816803981f
Emil Osmanbegović <hello@emilosman.com>
2024-07-18 14:16:02 +0200

    add terraform test script

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ terraform {
 provider "libvirt" {
   uri = "qemu:///session?socket=/Users/emiloszmanbegovity/.cache/libvirt/libvirt-sock"
 }
+
+resource "libvirt_domain" "terraform_test" {
+    name = "terraform_test"
+    memory = 512
+    vcpu   = 1
+    type   = "kvm"
+
+    # Specify the path to the QEMU emulator
+    emulator = "/opt/homebrew/bin/qemu-system-aarch64"
+
+    # Specify the machine type suitable for your architecture
+    machine = "virt"
+
+}
+
